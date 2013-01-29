0382020ba365f92329e341f6d4f005de4adad87d
whirlwin <whirlwin90@gmail.com>
2013-01-29 23:29:08 +0100

    Initial commit.

---
+++ whirl.js
@@ -0,0 +1,19 @@
+var whirl = (function() {
+
+  "use strict"
+
+  function ns(namespace) {
+    if (namespace == undefined) {
+      console.log('Please provide a namespace');
+    }
+
+    else if (!/^\w+(\.\w+)*$/.test(namespace)) {
+      console.log('Illegal namespace. Format: foo.bar.baz');
+    }
+  }
+
+  return {
+    ns: ns
+  }
+
+})();
