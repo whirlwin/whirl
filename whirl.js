9d531ac39b5b7c1cec2d09ea32b5f68f4e1345f4
whirlwin <whirlwin90@gmail.com>
2013-01-30 12:08:09 +0100

    Remade the core structure from scratch.

--- whirl.js
+++ whirl.js
@@ -1,18 +1,57 @@
 var whirl = (function() {
   "use strict";
 
-  function ns(namespace) {
-    if (namespace == undefined) {
-      console.log('Please provide a namespace');
-    }
-
-    else if (!/^\w+(\.\w+)*$/.test(namespace)) {
-      console.log('Illegal namespace. Format: foo.bar.baz');
+  function newClass(namespace, body) {
+    if (checkArguments(namespace, body)) {
+      console.log('good');
     }
   }
 
+  function checkArguments(namespace, body) {
+    var valid = true;
+
+
+    // Local functions ---------------------------------------------------------
+
+    function invalidate(message) {
+      valid = false;
+      console.log(message);
+    }
+
+    function checkNamespace(namespace) {
+      if (!/^\w+(\.\w+)*$/.test(namespace)) {
+        invalidate('Please provide a valid namespace. E.g. foo.bar.baz');
+      }
+
+      else if (window[namespace] != undefined) {
+        invalidate('The namespace is already taken');
+      }
+    }
+
+    function createClass(namespace) {
+      var identifiers = namespace.split('.');
+    }
+
+
+    // Checking ----------------------------------------------------------------
+
+    if (namespace == undefined || body == undefined) {
+      invalidate('Invalid arguments. The syntax for invoking newClass is: ' +
+        'whirl.newClass(namespace, body())');
+    }
+
+    else if (checkNamespace(namespace)) {}
+
+    else {
+      createClass(namespace);
+    }
+
+
+    return valid;
+  }
+
   return {
-    ns: ns
+    newClass: newClass
   }
 
 })();
