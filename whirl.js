c4833edb8d90b737df28e1646f2f9d0e695500f0
whirlwin <whirlwin90@gmail.com>
2013-02-05 09:37:36 +0100

    Removed unused code.

--- whirl.js
+++ whirl.js
@@ -31,58 +31,16 @@ var whirl = (function() {
     function createClass(namespace) {
       var identifiers = namespace.split('.');
 
-      var tmpJson = '';
-      var closing = '';
+      window[identifiers[0]] = (function createObject(array) {
+        if(array.length == 0) {
+          return body;
 
-      //window[identifiers[0]] = JSON.parse('{"' + identifiers[1] + '": {"' + identifiers[2] + '":24}}');
-
-      function createObject(o, array) {
-      if(array.length === 0) {
-        return body;
-
-        } else {
-          var obj = {};
-          obj[array[0]] = createObject(obj, array.splice(1));
-          return obj;
-        }
-      }
-
-      var a;
-      var o = createObject(a, identifiers);
-
-      window[identifiers[0]] = o;
-
-      return;
-      for (var i = 1; i < identifiers.length; i++) {
-        tmpJson += '{"' + identifiers[i] + '":';
-        closing += '}';
-      }
-
-      tmpJson += 'null' + closing;
-
-      var x = JSON.parse(tmpJson);
-
-      window[identifiers[0]] = x;
-
-      for (var i in window[identifiers[0]]) {
-        //console.log(i);
-      }
-
-      /*
-
-      var bd = body();
-
-      //console.log(window[identifiers[0]]);
-
-      var last = null;
-      var a = gd(window[identifiers[0]]);
-      */
-
-      function gd(obj) {
-        for (var i in obj) {
-          return obj[i] == null ? obj : gd(obj[i]);
-        }
-      }
+          } else {
+            var obj = {};
+            obj[array[0]] = createObject(array.splice(1));
+            return obj;
+          }
+        })(identifiers);
     }
 
 
